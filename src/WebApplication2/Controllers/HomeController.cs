c135dcc1a43a1cd840bd6edf77461a997c1d5043
Merlin VOTAT <m.votat@pickup-services.com>
2022-04-19 10:54:23 +0200

    master detail

--- src/WebApplication2/Controllers/HomeController.cs
+++ src/WebApplication2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataProvider;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +13,23 @@ namespace WebApplication2.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPersonRepository _dataProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPersonRepository personRepository)
         {
             _logger = logger;
+            _dataProvider = personRepository;
+
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_dataProvider.GetAll());
+        }
+
+        public IActionResult Detail(Guid id)
+        {
+            return View(_dataProvider.Get(id));
         }
 
         public IActionResult Privacy()
--- src/WebApplication2/Startup.cs
+++ src/WebApplication2/Startup.cs
@@ -1,8 +1,10 @@
+using DataProvider;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +24,8 @@ namespace WebApplication2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddSingleton<IPersonRepository>(new PersonRepository());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
