673eff9a82464df0e5bcccacc98b5d176f3e2542
Merlin VOTAT <m.votat@pickup-services.com>
2022-04-19 11:14:44 +0200

    master detail

---
+++ src/WebApplication3/Pages/Detail.cshtml.cs
@@ -0,0 +1,26 @@
+using DataProvider;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace WebApplication3.Pages
+{
+    public class DetailModel : PageModel
+    {
+        public Person person;
+        private readonly IPersonRepository _dataProvider;
+        private readonly ILogger<DetailModel> _logger;
+
+        public DetailModel(ILogger<DetailModel> logger, IPersonRepository dataProvider)
+        {
+            _dataProvider = dataProvider;
+            _logger = logger;
+        }
+
+        public void OnGet(Guid id)
+        {
+            person = _dataProvider.Get(id);
+        }
+    }
+}
